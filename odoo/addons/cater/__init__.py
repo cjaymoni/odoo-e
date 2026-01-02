49d594f0ca70f6ad2e2c5665ea228148f0f710cb
Jude Clottey <judeclottey1@gmail.com>
2026-01-02 19:38:00 +0000

    Remove outdated views and templates related to catering packages, portal functionalities, reports, services, and WhatsApp integration to streamline the codebase and improve maintainability.

--- odoo/addons/cater/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import models
-from . import controllers
-# Don't import tests module - it's loaded automatically by Odoo when running tests
--- odoo/addons/cater/tests/__init__.py
+++
@@ -1,5 +0,0 @@
-# Import all test modules to ensure they are discovered
-from . import test_catering_models
-from . import test_security
-from . import test_webhook_controllers
-from . import test_whatsapp_integration
